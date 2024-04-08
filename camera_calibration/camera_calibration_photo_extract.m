b97a9a152a66cf19d3076b23fa60f8e932970b57
qlandon <quentinlandon18@gmail.com>
2024-04-08 17:21:46 +0200

    Init projet

---
+++ camera_calibration/camera_calibration_photo_extract.m
@@ -0,0 +1,18 @@
+Tello = ryze("Tello");
+TelloCamera = camera(Tello);
+ImageFolder ='\Camera_calibration_img';
+
+
+for k=1:30
+    Image = snapshot(TelloCamera);
+    file_name = sprintf('Image%d.png',k);
+    imgName = fullfile(ImageFolder,file_name) ;
+    imwrite(Image,imgName);
+    imshow(Image);
+    pause(1);
+    
+end
+
+clear;
+
+%run cameraCalibrator size square = 34, 6X8
