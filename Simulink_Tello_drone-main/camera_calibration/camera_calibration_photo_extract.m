1e1e706007a3ca669b610ee7d8986a0b5553df98
QUENTIN LANDON <quentinlandon18@gmail.com>
2024-05-22 18:11:17 +0200

    maj

---
+++ Simulink_Tello_drone-main/camera_calibration/camera_calibration_photo_extract.m
@@ -0,0 +1,62 @@
+clear Tello
+
+Tello = ryze("Tello");
+TelloCamera = camera(Tello);
+% cam = webcam(1);
+ImageFolder ='C:\Users\user\OneDrive - ESTACA\Documents\DRONE_SEN4\Simulink_Tello_drone-main_V3.0\camera_calibration\Camera_calibration_6363AC';
+preview(TelloCamera)
+
+for k=1:30
+    Image = snapshot(TelloCamera);
+    file_name = sprintf('Image%d.png',k);
+    imgName = fullfile(ImageFolder,file_name) ;
+    imwrite(Image,imgName);
+    imshow(Image);
+    pause(1);
+
+end
+
+clear;
+
+%run cameraCalibrator size square = 34, 6X8
+
+% % Spécifiez le dossier contenant les images
+% input_folder = 'C:\Users\user\OneDrive - ESTACA\Documents\DRONE_SEN4\Simulink_Tello_drone-main\camera_calibration\Camera_calibration_img'; % Remplacez par le chemin de votre dossier
+% output_folder = fullfile(input_folder, 'resized_images');
+% 
+% % Créez le dossier de sortie s'il n'existe pas
+% if ~exist(output_folder, 'dir')
+%     mkdir(output_folder);
+% end
+% 
+% % Liste de tous les fichiers dans le dossier d'entrée
+% image_files = dir(fullfile(input_folder, '*.*'));
+% image_files = image_files(~[image_files.isdir]); % Exclure les dossiers
+% 
+% % Dimensions de redimensionnement
+% new_width = 480;
+% new_height = 360;
+% 
+% % Parcourir chaque fichier image
+% for i = 1:length(image_files)
+%     % Nom et chemin complet de l'image actuelle
+%     image_name = image_files(i).name;
+%     image_path = fullfile(input_folder, image_name);
+% 
+%     % Lire l'image
+%     try
+%         img = imread(image_path);
+% 
+%         % Redimensionner l'image
+%         resized_img = imresize(img, [new_height, new_width]);
+% 
+%         % Chemin de sortie de l'image redimensionnée
+%         output_path = fullfile(output_folder, image_name);
+% 
+%         % Enregistrer l'image redimensionnée
+%         imwrite(resized_img, output_path);
+%     end
+% 
+% end
+% 
+% disp('Redimensionnement terminé pour toutes les images.');
--- Simulink_Tello_drone-main_V3.0/Matlab_function/Yolo_create_mat_train.m
+++
@@ -1,3 +0,0 @@
-name = "tiny-yolov4-coco";
-yolov4ObjectDetector(name);
-save('tinyyolov4coco.mat');
