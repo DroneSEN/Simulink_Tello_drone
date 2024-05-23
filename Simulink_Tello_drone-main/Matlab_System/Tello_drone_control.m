795820e80990058dc189c4ca76f89632533cdb6b
QUENTIN LANDON <quentinlandon18@gmail.com>
2024-05-23 19:20:27 +0200

    Correction Tello_control

--- Simulink_Tello_drone-main/Matlab_System/Tello_drone_control.m
+++ Simulink_Tello_drone-main/Matlab_System/Tello_drone_control.m
@@ -16,6 +16,7 @@ classdef Tello_drone_control < matlab.System
         imgFront;       % Image capturée de la caméra frontale
         imgDown;        % Image capturée de la caméra inférieure
         hasTakenOff;    % Statut du décollage
+        previousRcEnabled = 0; % Flag pour détecter un front montant/descendant sur le RC Enabled
         SizeImageFront = [720, 960, 3]; % Taille de l'image par défaut pour la caméra frontale
         SizeImageDown = [480, 640, 3]; % Taille de l'image par défaut pour la caméra inférieure
     end
@@ -34,7 +35,7 @@ classdef Tello_drone_control < matlab.System
             obj.hasTakenOff = false; % Initialisation du statut de décollage
         end
 
-        function [imageFront, imageDown, Eulerangles, speedXYZ] = stepImpl(obj, Slam, tkoff, landing, xMove, yMove, zMove, degree, movexyz, moverotate, downcam)
+        function [imageFront, imageDown, Eulerangles, speedXYZ, accelXYZ] = stepImpl(obj, Slam, tkoff, landing, xMove, yMove, zMove, degree, movexyz, moverotate,enableRC,rcspeeds, downcam)
             % Capturez l'image si le commutateur Slam est activé
             if Slam == 1
                 if downcam == 1
@@ -80,6 +81,25 @@ classdef Tello_drone_control < matlab.System
             % Lecture des angles d'Euler et de la vitesse
             [Eulerangles, ~] = readOrientation(obj.drone);
             [speedXYZ, ~] = readSpeed(obj.drone);
+            % Commande RC
+            if enableRC == 1
+                rc(obj.drone, rcspeeds(2), rcspeeds(1), rcspeeds(3), rcspeeds(4));
+            end
+
+            % Envoie d'une commande RC (0,0,0,0) lorsque l'on désactive le
+            % RC
+            if enableRC == 0 && obj.previousRcEnabled == 1
+                rc(obj.drone, 0, 0, 0, 0);
+            end
+
+            [accelXYZ, ~] = readAccel(obj.drone);
+
+            % Store previous value
+            obj.previousRcEnabled = enableRC;
+
+           
+            % Lecture des angles d'Euler et de la vitesse
+
         end
 
         function resetImpl(obj)
@@ -94,41 +114,46 @@ classdef Tello_drone_control < matlab.System
         end
 
         %% Fonctions Simulink
-        function [out1, out2, out3, out4] = getOutputSizeImpl(obj)
+        function [out1, out2, out3, out4,out5] = getOutputSizeImpl(obj)
             % Retourne la taille pour chaque port de sortie
             out1 = obj.SizeImageFront; % Taille dynamique de l'image frontale
             out2 = obj.SizeImageDown;  % Taille dynamique de l'image inférieure
             out3 = [1, 3]; % Angles ZYX
             out4 = [1, 3]; % Vitesse XYZ
+            out5 = [1, 3]; % Accélération XYZ
         end
 
-        function [out1, out2, out3, out4] = getOutputDataTypeImpl(obj)
+        function [out1, out2, out3, out4, out5] = getOutputDataTypeImpl(obj)
             % Retourne le type de données pour chaque port de sortie
             out1 = "uint8";
             out2 = "uint8";
             out3 = 'double';
             out4 = 'double';
+            out5 = 'double';
+
         end
 
-        function [out1, out2, out3, out4] = isOutputComplexImpl(obj)
+        function [out1, out2, out3, out4, out5] = isOutputComplexImpl(obj)
             % Retourne false pour chaque port de sortie avec des données non complexes
             out1 = false;
             out2 = false;
             out3 = false;
             out4 = false;
+            out5 = false;
         end
 
-        function [out1, out2, out3, out4] = isOutputFixedSizeImpl(obj)
+        function [out1, out2, out3, out4, out5] = isOutputFixedSizeImpl(obj)
             % Retourne true pour chaque port de sortie avec une taille fixe
             out1 = true; % Taille dynamique pour l'image frontale
             out2 = true; % Taille dynamique pour l'image inférieure
             out3 = true;
             out4 = true;
+            out5 = true;
         end
 
         function num = getNumInputsImpl(obj)
             % Définissez le nombre d'entrées du système
-            num = 10; % Slam, takeoff, land et les valeurs de déplacement
+            num = 12; % Slam, takeoff, land et les valeurs de déplacement
         end
 
         function num = getNumOutputsImpl(obj)
