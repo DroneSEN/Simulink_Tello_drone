894be4f69eb28198868610ea3273c0ea067dd0f0
SashaCtp <sasha.contrepois@hotmail.fr>
2024-06-12 08:56:23 +0200

    Amélioration de gestion caméra

--- Simulink_Tello_drone-main/Matlab_System/Tello_drone_control.m
+++ Simulink_Tello_drone-main/Matlab_System/Tello_drone_control.m
@@ -15,10 +15,14 @@ classdef Tello_drone_control < matlab.System
         cam;            % Caméra du drone
         imgFront;       % Image capturée de la caméra frontale
         imgDown;        % Image capturée de la caméra inférieure
+        imgBoth;        % Image capturée des deux caméras
         hasTakenOff;    % Statut du décollage
         previousRcEnabled = 0; % Flag pour détecter un front montant/descendant sur le RC Enabled
         SizeImageFront = [720, 960, 3]; % Taille de l'image par défaut pour la caméra frontale
-        SizeImageDown = [480, 640, 3]; % Taille de l'image par défaut pour la caméra inférieure
+        SizeImageDown = [240, 320, 3]; % Taille de l'image par défaut pour la caméra inférieure
+        SizeImageMix = [1000, 1000, 3]; % Taille de l'image avec les deux vidéos
+        prvRoundedRcSpeeds = [0; 0; 0; 0]; % Dernières valeurs de la commande RC
+        CurrentVideoDirection = VideoDirection.Forward; % Direction de la caméra
     end
 
     methods (Access = protected)
@@ -32,28 +36,55 @@ classdef Tello_drone_control < matlab.System
             obj.cam = camera(obj.drone);
             obj.imgFront = zeros(obj.SizeImageFront, 'uint8');
             obj.imgDown = zeros(obj.SizeImageDown, 'uint8');
+            obj.imgBoth = zeros(obj.SizeImageFront, 'uint8');
             obj.hasTakenOff = false; % Initialisation du statut de décollage
+
+            % Activation des mission pads
+            % activateMissionPad(obj.drone)
         end
 
-        function [imageFront, imageDown, Eulerangles, speedXYZ, accelXYZ] = stepImpl(obj, Slam, tkoff, landing, xMove, yMove, zMove, degree, movexyz, moverotate,enableRC,rcspeeds, downcam)
-            % Capturez l'image si le commutateur Slam est activé
-            if Slam == 1
-                if downcam == 1
-                    switch_camera(obj.drone, "down");
-                    image = snapshot(obj.cam);
-                    if ~isempty(image)
-                        obj.imgDown = image;
-                    end
-                else
-                    switch_camera(obj.drone, "front");
-                    image = snapshot(obj.cam);
-                    if ~isempty(image)
-                        obj.imgFront = image;
+        function [imageFront, imageDown, imageBoth, Eulerangles, speedXYZ, accelXYZ, batteryLevel, currentVideoDirection] = stepImpl(obj, enableVideo, tkoff, landing, xMove, yMove, zMove, degree, movexyz, moverotate,enableRC,rcspeeds, cmdVideoDirection)
+            % Gestion de la video si activé
+            if enableVideo == 1
+
+                % Récupération de l'image
+                image = snapshot(obj.cam);
+
+                % On recopie l'image dans l'attribut associée
+                if ~isempty(image)
+                    switch obj.CurrentVideoDirection
+                        case VideoDirection.Forward
+                            if ~any(size(image) - obj.SizeImageFront)
+                                obj.imgFront = image;
+                            end
+                        case VideoDirection.Downward
+                            if ~any(size(image) - obj.SizeImageDown)
+                                % Si la taille est correcte, on copie
+                                % l'image
+                                obj.imgDown = image;
+                            end
+                        case VideoDirection.Both
+                            % Split de l'image
+                            s = size(image);
+                            obj.imgBoth(1:s(1), 1:s(2), :) = image;
                     end
                 end
+                
+                % Si la direction de la caméra change depuis le dernier
+                % cycle, on envoit une commande de changement
+                if obj.CurrentVideoDirection ~= cmdVideoDirection
+                    
+                    switch_camera(obj.drone, uint16(cmdVideoDirection));
+
+                    % On sauvegarde la direction actuelle
+                    obj.CurrentVideoDirection = cmdVideoDirection;
+                end
             end
+    
+            % Output the data
             imageFront = obj.imgFront;
             imageDown = obj.imgDown;
+            imageBoth = obj.imgBoth;
 
             % Gestion du décollage
             if tkoff == 1 && ~obj.hasTakenOff
@@ -81,24 +112,40 @@ classdef Tello_drone_control < matlab.System
             % Lecture des angles d'Euler et de la vitesse
             [Eulerangles, ~] = readOrientation(obj.drone);
             [speedXYZ, ~] = readSpeed(obj.drone);
-            % Commande RC
+
+            % Envoie de la commande RC
             if enableRC == 1
-                rc(obj.drone, rcspeeds(2), rcspeeds(1), rcspeeds(3), rcspeeds(4));
+                roundedRcSpeeds = round(rcspeeds);
+
+                % On envoie la commande si elle diffère de la précédente 
+                if any(roundedRcSpeeds-obj.prvRoundedRcSpeeds)
+                    rc(obj.drone, int16(roundedRcSpeeds(2)), ...
+                                  int16(roundedRcSpeeds(1)), ...
+                                  int16(roundedRcSpeeds(3)), ...
+                                  int16(roundedRcSpeeds(4)));
+                    obj.prvRoundedRcSpeeds = roundedRcSpeeds;
+                end
             end
 
             % Envoie d'une commande RC (0,0,0,0) lorsque l'on désactive le
             % RC
             if enableRC == 0 && obj.previousRcEnabled == 1
                 rc(obj.drone, 0, 0, 0, 0);
+                obj.prvRoundedRcSpeeds = [0;0;0;0];
             end
 
             [accelXYZ, ~] = readAccel(obj.drone);
 
             % Store previous value
             obj.previousRcEnabled = enableRC;
-
            
             % Lecture des angles d'Euler et de la vitesse
+
+            % Renvoie de la batterie
+            batteryLevel = readBattery(obj.drone);
+
+            % Renvoie de la direction de caméra
+            currentVideoDirection = uint16(obj.CurrentVideoDirection);
 
         end
 
@@ -114,41 +161,56 @@ classdef Tello_drone_control < matlab.System
         end
 
         %% Fonctions Simulink
-        function [out1, out2, out3, out4,out5] = getOutputSizeImpl(obj)
+        function [out1, out2, out3, out4,out5, out6, out7, out8] = getOutputSizeImpl(obj)
             % Retourne la taille pour chaque port de sortie
             out1 = obj.SizeImageFront; % Taille dynamique de l'image frontale
             out2 = obj.SizeImageDown;  % Taille dynamique de l'image inférieure
-            out3 = [1, 3]; % Angles ZYX
-            out4 = [1, 3]; % Vitesse XYZ
-            out5 = [1, 3]; % Accélération XYZ
+            out3 = obj.SizeImageMix;  % Taille dynamique des deux images
+            
+            out4 = [1, 3]; % Angles ZYX
+            out5 = [1, 3]; % Vitesse XYZ
+            out6 = [1, 3]; % Accélération XYZ
+            out7 = [1, 1]; % Niveau de batterie
+
+            out8 = [1, 1]; % Direction actuelle de la vidéo
         end
 
-        function [out1, out2, out3, out4, out5] = getOutputDataTypeImpl(obj)
+        function [out1, out2, out3, out4, out5, out6, out7, out8] = getOutputDataTypeImpl(obj)
             % Retourne le type de données pour chaque port de sortie
             out1 = "uint8";
             out2 = "uint8";
-            out3 = 'double';
+            out3 = "uint8";
+
             out4 = 'double';
             out5 = 'double';
+            out6 = 'double';
+            out7 = 'double';
 
+            out8 = "uint16"; % Direction actuelle de la vidéo
         end
 
-        function [out1, out2, out3, out4, out5] = isOutputComplexImpl(obj)
+        function [out1, out2, out3, out4, out5, out6, out7, out8] = isOutputComplexImpl(obj)
             % Retourne false pour chaque port de sortie avec des données non complexes
             out1 = false;
             out2 = false;
             out3 = false;
             out4 = false;
             out5 = false;
+            out6 = false;
+            out7 = false;
+            out8 = false;
         end
 
-        function [out1, out2, out3, out4, out5] = isOutputFixedSizeImpl(obj)
+        function [out1, out2, out3, out4, out5, out6, out7, out8] = isOutputFixedSizeImpl(obj)
             % Retourne true pour chaque port de sortie avec une taille fixe
             out1 = true; % Taille dynamique pour l'image frontale
-            out2 = true; % Taille dynamique pour l'image inférieure
-            out3 = true;
+            out2 = false; % Taille dynamique pour l'image inférieure
+            out3 = false;
             out4 = true;
             out5 = true;
+            out6 = true;
+            out7 = true;
+            out8 = true;
         end
 
         function num = getNumInputsImpl(obj)
@@ -158,7 +220,7 @@ classdef Tello_drone_control < matlab.System
 
         function num = getNumOutputsImpl(obj)
             % Définissez le nombre de sorties du système
-            num = 4; % Image frontale, image inférieure, angles Euler, vitesse XYZ
+            num = 8; % Images (frontales, inférieures, mix), angles Euler, vitesse XYZ, direction caméra
         end
     end
 end
---
+++ Simulink_Tello_drone-main/Matlab_System/VideoDirection.m
@@ -0,0 +1,7 @@
+classdef VideoDirection < uint16
+    enumeration
+        Forward (0)
+        Downward (1)
+        Both (2)
+    end
+end
