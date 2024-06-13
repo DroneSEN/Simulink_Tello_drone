49e20aaa88a7f72045eaa91f389ca81189a72600
qlandon <quentinlandon18@gmail.com>
2024-06-13 16:14:55 +0200

    optimisation +commentaire FR (yolo) ; supression fichier inutile

--- Simulink_Tello_drone-main/Matlab_System/Yolo_Object_Detector.m
+++ Simulink_Tello_drone-main/Matlab_System/Yolo_Object_Detector.m
@@ -1,5 +1,6 @@
 classdef Yolo_Object_Detector < matlab.System
     % Yolo_Object_Detector - Détecte des objets et calcule leur position en XYZ
+
     properties (Access = private)
         yolo % Détecteur YOLO
         focalLength % Longueur focale de la caméra
@@ -9,12 +10,16 @@ classdef Yolo_Object_Detector < matlab.System
     methods (Access = protected)
         %% Initialisation
         function setupImpl(obj)
+            % Nom du modèle YOLO utilisé
             name = 'tiny-yolov4-coco';
-            % Initialiser YOLO
+            
+            % Initialiser le détecteur YOLO
             obj.yolo = yolov4ObjectDetector(name);
-            obj.focalLength = [903, 906]; % Exemple de focal length
-
-            % Charger les dimensions moyennes des objets à partir du fichier
+            
+            % Définir la longueur focale de la caméra (exemple de valeurs)
+            obj.focalLength = [903, 906];
+            
+            % Charger les dimensions moyennes des objets à partir d'un fichier
             data = load('Matlab_System/objectDimensions.mat');
             obj.objectDimensions = data.objectDimensions;
         end
@@ -25,23 +30,20 @@ classdef Yolo_Object_Detector < matlab.System
             % translationMatrix : matrice de translation 3x1 du drone
             % rotationMatrix : matrice de rotation 3x3 du drone
 
-            % Détecter les objets
+            % Détecter les objets dans l'image
             [bboxs, ~, labels] = detect(obj.yolo, I, 'Threshold', 0.1);
 
-            % Convertir les labels en tableau de cellules
+            % Convertir les labels en tableau de cellules si nécessaire
             if ~iscell(labels)
                 labels = cellstr(labels);
             end
 
-            % Initialiser les positions des objets et leurs dimensions
+            % Initialiser les positions des objets et leurs distances
             objectXYZ = zeros(100, 3);
-            distances = zeros(100, 1); % Tableau pour stocker les distances
+            distances = zeros(100, 1);
             objectColors = repmat("yellow", size(bboxs, 1), 1);
 
-            % Taille de l'image d'entrée
-            %[imgHeight, imgWidth, ~] = size(I);
-
-            % Calculer la distance pour chaque objet détecté et ajouter l'annotation
+            % Parcourir chaque objet détecté
             for i = 1:size(bboxs, 1)
                 % Récupérer la hauteur et la largeur de la boîte englobante
                 bboxHeight = bboxs(i, 4);
@@ -52,21 +54,20 @@ classdef Yolo_Object_Detector < matlab.System
                     dimensions = obj.objectDimensions.(labels{i});
                     realHeight = dimensions(1);
                     realWidth = dimensions(2);
-                    % realDepth = dimensions(3);
 
-                    % Calculer la distance en utilisant la largeur de la boîte
+                    % Calculer la distance en utilisant la largeur et la hauteur de la boîte englobante
                     distanceW = (realWidth * obj.focalLength(1)) / bboxWidth;
                     distanceH = (realHeight * obj.focalLength(1)) / bboxHeight;
                     distance = (distanceW + distanceH) / 2;
 
+                    % Stocker la distance
+                    distances(i) = distance;
 
-                    distances(i) = distance; % Stocker la distance
-
-                    % Calculer le vecteur de direction
+                    % Calculer le vecteur de direction dans le référentiel global
                     direction_local = [1; 0; 0]; % Vecteur unitaire dans le référentiel de l'objet
                     direction_global = rotationMatrix * direction_local;
-                    
-                    % Calculer la position de l'objet
+
+                    % Calculer la position de l'objet en XYZ
                     objectPos = translationMatrix + distance * direction_global;
 
                     % Appliquer les transformations de référentiel
@@ -75,10 +76,10 @@ classdef Yolo_Object_Detector < matlab.System
                     % Stocker la position de l'objet transformée
                     objectXYZ(i, :) = transformedPos';
 
-                    % Annoter l'image
+                    % Annoter l'image avec la distance et la position
                     labels{i} = sprintf('%s (%.2f m, [%.2f, %.2f, %.2f])', labels{i}, distance, transformedPos(1), transformedPos(2), transformedPos(3));
 
-                    % Déterminer la couleur en fonction de la distance
+                    % Déterminer la couleur de l'annotation en fonction de la distance
                     if distance > 3
                         objectColors(i) = 'green';
                     elseif distance > 1
@@ -95,7 +96,7 @@ classdef Yolo_Object_Detector < matlab.System
             % Supprimer les lignes non utilisées des objets détectés
             nonZeroIdx = any(objectXYZ ~= 0, 2);
             objectXYZ = objectXYZ(nonZeroIdx, :);
-            distances = distances(nonZeroIdx, :); % Supprimer les lignes non utilisées
+            distances = distances(nonZeroIdx, :);
 
             % Remplir les variables avec des zéros pour assurer une taille fixe de [100, 3] pour objectXYZ et [100, 1] pour distances
             objectXYZ = [objectXYZ; zeros(100 - size(objectXYZ, 1), 3)];
@@ -104,7 +105,7 @@ classdef Yolo_Object_Detector < matlab.System
 
         %% Définir les tailles des sorties
         function [out1, out2, out3] = getOutputSizeImpl(~)
-            % Return size for each output port
+            % Retourner la taille de chaque port de sortie
             out1 = [100, 3]; % objectXYZ
             out2 = [100, 1]; % distance
             out3 = [720, 960, 3]; % annotatedImage
@@ -112,7 +113,7 @@ classdef Yolo_Object_Detector < matlab.System
 
         %% Définir les types de données des sorties
         function [out1, out2, out3] = getOutputDataTypeImpl(~)
-            % Return data type for each output port
+            % Retourner le type de données de chaque port de sortie
             out1 = 'double'; % objectXYZ
             out2 = 'double'; % distance
             out3 = 'uint8'; % annotatedImage
@@ -120,7 +121,7 @@ classdef Yolo_Object_Detector < matlab.System
 
         %% Définir si les sorties sont complexes
         function [out1, out2, out3] = isOutputComplexImpl(~)
-            % Return true for each output port with complex data
+            % Retourner vrai pour chaque port de sortie avec des données complexes
             out1 = false; % objectXYZ
             out2 = false; % distance
             out3 = false; % annotatedImage
@@ -128,7 +129,7 @@ classdef Yolo_Object_Detector < matlab.System
 
         %% Définir si les tailles des sorties sont fixes
         function [out1, out2, out3] = isOutputFixedSizeImpl(~)
-            % Return true for each output port with fixed size
+            % Retourner vrai pour chaque port de sortie avec une taille fixe
             out1 = true; % objectXYZ
             out2 = true; % distance
             out3 = true; % annotatedImage
@@ -136,7 +137,7 @@ classdef Yolo_Object_Detector < matlab.System
 
         %% Définir les noms des ports d'entrée
         function [name1, name2, name3] = getInputNamesImpl(~)
-            % Return input port names for System block
+            % Retourner les noms des ports d'entrée pour le bloc système
             name1 = 'Image';
             name2 = 'TranslationMatrix';
             name3 = 'RotationMatrix';
@@ -144,7 +145,7 @@ classdef Yolo_Object_Detector < matlab.System
 
         %% Définir les noms des ports de sortie
         function [name1, name2, name3] = getOutputNamesImpl(~)
-            % Return output port names for System block
+            % Retourner les noms des ports de sortie pour le bloc système
             name1 = 'ObjectXYZ';
             name2 = 'Distance';
             name3 = 'AnnotatedImage';
