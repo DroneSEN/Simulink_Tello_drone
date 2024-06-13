130f4d5ae347b791f3cefcd1b1cc9e52cec68dcb
qlandon <quentinlandon18@gmail.com>
2024-06-13 14:57:23 +0200

    optimisation tello_control (+commentaire FR)

--- Simulink_Tello_drone-main/Matlab_System/Tello_drone_control.m
+++ Simulink_Tello_drone-main/Matlab_System/Tello_drone_control.m
@@ -6,7 +6,7 @@ classdef Tello_drone_control < matlab.System
 
     % Propriétés publiques et modifiables
     properties
-        droneIP = "127.0.0.1";
+        droneIP = "127.0.0.1"; % Adresse IP du drone (modifiable)
     end
 
     % Propriétés privées
@@ -15,19 +15,17 @@ classdef Tello_drone_control < matlab.System
         cam;            % Caméra du drone
         imgFront;       % Image capturée de la caméra frontale
         imgDown;        % Image capturée de la caméra inférieure
-        imgBoth;        % Image capturée des deux caméras
         hasTakenOff;    % Statut du décollage
-        previousRcEnabled = 0; % Flag pour détecter un front montant/descendant sur le RC Enabled
+        previousRcEnabled = 0; % Flag pour détecter un changement dans l'activation du RC
         SizeImageFront = [720, 960, 3]; % Taille de l'image par défaut pour la caméra frontale
         SizeImageDown = [240, 320, 3]; % Taille de l'image par défaut pour la caméra inférieure
-        SizeImageMix = [1000, 1000, 3]; % Taille de l'image avec les deux vidéos
         prvRoundedRcSpeeds = [0; 0; 0; 0]; % Dernières valeurs de la commande RC
         CurrentVideoDirection = VideoDirection.Forward; % Direction de la caméra
     end
 
     methods (Access = protected)
         function setupImpl(obj)
-            % Initialisez la connexion avec le drone et la caméra
+            % Initialisation de la connexion avec le drone et la caméra
             if obj.droneIP == "127.0.0.1" || isempty(obj.droneIP)
                 obj.drone = ryze("Tello");
             else
@@ -36,160 +34,115 @@ classdef Tello_drone_control < matlab.System
             obj.cam = camera(obj.drone);
             obj.imgFront = zeros(obj.SizeImageFront, 'uint8');
             obj.imgDown = zeros(obj.SizeImageDown, 'uint8');
-            obj.imgBoth = zeros(obj.SizeImageFront, 'uint8');
             obj.hasTakenOff = false; % Initialisation du statut de décollage
-
-            % Activation des mission pads
-            % activateMissionPad(obj.drone)
         end
 
-        function [imageFront, imageDown, imageBoth, Eulerangles, speedXYZ, accelXYZ, batteryLevel, currentVideoDirection] = stepImpl(obj, enableVideo, tkoff, landing, xMove, yMove, zMove, degree, movexyz, moverotate,enableRC,rcspeeds, cmdVideoDirection)
-            % Gestion de la video si activé
-            if enableVideo == 1
+        function [imageFront, imageDown, Eulerangles, speedXYZ, batteryLevel, currentVideoDirection] = stepImpl(obj, enableVideo, tkoff, landing, enableRC, rcspeeds, cmdVideoDirection, xMove, yMove, zMove, degree, movexyz, moverotate)
+            % Gestion de la vidéo si activée
+            if enableVideo
+                image = snapshot(obj.cam); % Récupération de l'image de la caméra
 
-                % Récupération de l'image
-                image = snapshot(obj.cam);
-
-                % On recopie l'image dans l'attribut associée
+                % Mise à jour de l'image capturée en fonction de la direction de la caméra
                 if ~isempty(image)
-                    switch obj.CurrentVideoDirection
-                        case VideoDirection.Forward
-                            if ~any(size(image) - obj.SizeImageFront)
-                                obj.imgFront = image;
-                            end
-                        case VideoDirection.Downward
-                            if ~any(size(image) - obj.SizeImageDown)
-                                % Si la taille est correcte, on copie
-                                % l'image
-                                obj.imgDown = image;
-                            end
-                        case VideoDirection.Both
-                            % Split de l'image
-                            s = size(image);
-                            obj.imgBoth(1:s(1), 1:s(2), :) = image;
+                    if obj.CurrentVideoDirection == VideoDirection.Forward && ~any(size(image) - obj.SizeImageFront)
+                        obj.imgFront = image;
+                    elseif obj.CurrentVideoDirection == VideoDirection.Downward && ~any(size(image) - obj.SizeImageDown)
+                        obj.imgDown = image;
                     end
                 end
-                
-                % Si la direction de la caméra change depuis le dernier
-                % cycle, on envoit une commande de changement
-                if obj.CurrentVideoDirection ~= cmdVideoDirection
-                    
-                    switch_camera(obj.drone, uint16(cmdVideoDirection));
 
-                    % On sauvegarde la direction actuelle
-                    obj.CurrentVideoDirection = cmdVideoDirection;
+                % Commande de changement de direction de la caméra si nécessaire
+                if obj.CurrentVideoDirection ~= cmdVideoDirection
+                    switch_camera(obj.drone, uint16(cmdVideoDirection));
+                    obj.CurrentVideoDirection = cmdVideoDirection; % Mise à jour de la direction actuelle
                 end
             end
-    
-            % Output the data
+
+            % Assignation des images aux sorties
             imageFront = obj.imgFront;
             imageDown = obj.imgDown;
-            imageBoth = obj.imgBoth;
 
             % Gestion du décollage
-            if tkoff == 1 && ~obj.hasTakenOff
+            if tkoff && ~obj.hasTakenOff
                 takeoff(obj.drone, "WaitUntilDone", false);
                 obj.hasTakenOff = true;
             end
 
             % Gestion de l'atterrissage
-            if landing == 1 && obj.hasTakenOff
+            if landing && obj.hasTakenOff
                 land(obj.drone, "WaitUntilDone", false);
                 obj.hasTakenOff = false;
             end
 
-            % Déplacement du drone
-            if movexyz == 1 
-                moveValues = [xMove, yMove, zMove];
-                move(obj.drone, moveValues, "WaitUntilDone", false);
-            end
-
-            % Rotation du drone
-            if moverotate == 1
-                turn(obj.drone, deg2rad(degree), "WaitUntilDone", false);
-            end
+            % % Déplacement du drone
+            % if movexyz
+            %     move(obj.drone, [xMove, yMove, zMove], "WaitUntilDone", false);
+            % end
+            % 
+            % % Rotation du drone
+            % if moverotate
+            %     turn(obj.drone, deg2rad(degree), "WaitUntilDone", false);
+            % end
 
             % Lecture des angles d'Euler et de la vitesse
             [Eulerangles, ~] = readOrientation(obj.drone);
             [speedXYZ, ~] = readSpeed(obj.drone);
 
-            % Envoie de la commande RC
-            if enableRC == 1
+            % Envoi de la commande RC si activée
+            if enableRC
                 roundedRcSpeeds = round(rcspeeds);
-
-                % On envoie la commande si elle diffère de la précédente 
-                if any(roundedRcSpeeds-obj.prvRoundedRcSpeeds)
-                    rc(obj.drone, int16(roundedRcSpeeds(2)), ...
-                                  int16(roundedRcSpeeds(1)), ...
-                                  int16(roundedRcSpeeds(3)), ...
-                                  int16(roundedRcSpeeds(4)));
+                if any(roundedRcSpeeds - obj.prvRoundedRcSpeeds)
+                    rc(obj.drone, int16(roundedRcSpeeds(2)), int16(roundedRcSpeeds(1)), int16(roundedRcSpeeds(3)), int16(roundedRcSpeeds(4)));
                     obj.prvRoundedRcSpeeds = roundedRcSpeeds;
                 end
+            elseif obj.previousRcEnabled
+                rc(obj.drone, 0, 0, 0, 0); % Arrêt de la commande RC si désactivée
+                obj.prvRoundedRcSpeeds = [0; 0; 0; 0];
             end
 
-            % Envoie d'une commande RC (0,0,0,0) lorsque l'on désactive le
-            % RC
-            if enableRC == 0 && obj.previousRcEnabled == 1
-                rc(obj.drone, 0, 0, 0, 0);
-                obj.prvRoundedRcSpeeds = [0;0;0;0];
-            end
-
-            [accelXYZ, ~] = readAccel(obj.drone);
-
-            % Store previous value
-            obj.previousRcEnabled = enableRC;
-           
-            % Lecture des angles d'Euler et de la vitesse
-
-            % Renvoie de la batterie
+            % Lecture du niveau de batterie
             batteryLevel = readBattery(obj.drone);
 
-            % Renvoie de la direction de caméra
+            % Renvoi de la direction actuelle de la caméra
             currentVideoDirection = uint16(obj.CurrentVideoDirection);
 
+            % Mise à jour de l'état précédent du RC
+            obj.previousRcEnabled = enableRC;
         end
 
         function resetImpl(obj)
-            % Réinitialisez les propriétés internes
+            % Réinitialisation des propriétés internes
             obj.hasTakenOff = false;
         end
 
         function releaseImpl(obj)
-            % Fermez la connexion à la caméra et au drone
+            % Fermeture de la connexion à la caméra et au drone
             clear obj.cam;
             clear obj.drone;
         end
 
         %% Fonctions Simulink
-        function [out1, out2, out3, out4,out5, out6, out7, out8] = getOutputSizeImpl(obj)
+        function [out1, out2, out3, out4, out5, out6] = getOutputSizeImpl(obj)
             % Retourne la taille pour chaque port de sortie
-            out1 = obj.SizeImageFront; % Taille dynamique de l'image frontale
-            out2 = obj.SizeImageDown;  % Taille dynamique de l'image inférieure
-            out3 = obj.SizeImageMix;  % Taille dynamique des deux images
-            
-            out4 = [1, 3]; % Angles ZYX
-            out5 = [1, 3]; % Vitesse XYZ
-            out6 = [1, 3]; % Accélération XYZ
-            out7 = [1, 1]; % Niveau de batterie
-
-            out8 = [1, 1]; % Direction actuelle de la vidéo
+            out1 = obj.SizeImageFront; % Taille de l'image frontale
+            out2 = obj.SizeImageDown;  % Taille de l'image inférieure
+            out3 = [1, 3]; % Angles ZYX
+            out4 = [1, 3]; % Vitesse XYZ
+            out5 = [1, 1]; % Niveau de batterie
+            out6 = [1, 1]; % Direction actuelle de la vidéo
         end
 
-        function [out1, out2, out3, out4, out5, out6, out7, out8] = getOutputDataTypeImpl(obj)
+        function [out1, out2, out3, out4, out5, out6] = getOutputDataTypeImpl(obj)
             % Retourne le type de données pour chaque port de sortie
-            out1 = "uint8";
-            out2 = "uint8";
-            out3 = "uint8";
-
-            out4 = 'double';
-            out5 = 'double';
-            out6 = 'double';
-            out7 = 'double';
-
-            out8 = "uint16"; % Direction actuelle de la vidéo
+            out1 = "uint8";    % Type de données pour l'image frontale
+            out2 = "uint8";    % Type de données pour l'image inférieure
+            out3 = 'double';   % Type de données pour les angles d'Euler
+            out4 = 'double';   % Type de données pour la vitesse
+            out5 = 'double';   % Type de données pour le niveau de batterie
+            out6 = 'uint16';   % Type de données pour la direction de la caméra
         end
 
-        function [out1, out2, out3, out4, out5, out6, out7, out8] = isOutputComplexImpl(obj)
+        function [out1, out2, out3, out4, out5, out6] = isOutputComplexImpl(obj)
             % Retourne false pour chaque port de sortie avec des données non complexes
             out1 = false;
             out2 = false;
@@ -197,30 +150,26 @@ classdef Tello_drone_control < matlab.System
             out4 = false;
             out5 = false;
             out6 = false;
-            out7 = false;
-            out8 = false;
         end
 
-        function [out1, out2, out3, out4, out5, out6, out7, out8] = isOutputFixedSizeImpl(obj)
+        function [out1, out2, out3, out4, out5, out6] = isOutputFixedSizeImpl(obj)
             % Retourne true pour chaque port de sortie avec une taille fixe
-            out1 = true; % Taille dynamique pour l'image frontale
-            out2 = false; % Taille dynamique pour l'image inférieure
-            out3 = false;
+            out1 = true;  % Taille fixe pour l'image frontale
+            out2 = true;  % Taille fixe pour l'image inférieure
+            out3 = true;
             out4 = true;
             out5 = true;
             out6 = true;
-            out7 = true;
-            out8 = true;
         end
 
         function num = getNumInputsImpl(obj)
             % Définissez le nombre d'entrées du système
-            num = 12; % Slam, takeoff, land et les valeurs de déplacement
+            num = 6; % Nombre d'entrées attendu
         end
 
         function num = getNumOutputsImpl(obj)
             % Définissez le nombre de sorties du système
-            num = 8; % Images (frontales, inférieures, mix), angles Euler, vitesse XYZ, direction caméra
+            num = 6; % Nombre de sorties attendu
         end
     end
 end
