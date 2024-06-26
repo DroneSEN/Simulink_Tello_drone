271b596f5edffce1a2bb3ad85c9d5b719b9b9626
qlandon <quentinlandon18@gmail.com>
2024-06-26 15:18:28 +0200

    Fix erreur WaypointManager

--- Simulink_Tello_drone-main/Matlab_System/WaypointManager.m
+++ Simulink_Tello_drone-main/Matlab_System/WaypointManager.m
@@ -48,7 +48,7 @@ classdef WaypointManager < matlab.System
                 if (errorPos <= lookaheadDistance && obj.ElapsedTimeWaypoint >= requiredTime) || (obj.PrvForceNextWaypoint ~= forceNextWaypoint && forceNextWaypoint == 1)
                     
                     % On incrémente le temps passé sur le waypoint
-                    obj.ElapsedTimedWaypoint = obj.ElapsedTimedWaypoint + sampleTime;
+                    obj.ElapsedTimeWaypoint = obj.ElapsedTimeWaypoint + sampleTime;
 
                     % S'il ne s'âgit pas de la dernière position, on passe
                     % à la suivante
@@ -80,6 +80,34 @@ classdef WaypointManager < matlab.System
             % On retourne le waypoint actuel
             finished = obj.Finished;
             waypoint = waypoints(obj.CurrentWaypointIndex, 1:3);
+        end
+
+        function [waypoint, finished, elapsedTimeWaypoint] = getOutputSizeImpl(obj)
+            % Retourne la taille pour chaque port de sortie
+            waypoint = [1, 3];
+            finished = [1, 1]; 
+            elapsedTimeWaypoint = [1, 1];
+        end
+
+        function [waypoint, finished, elapsedTimeWaypoint] = getOutputDataTypeImpl(obj)
+            % Retourne le type de données pour chaque port de sortie
+            waypoint = "double"; 
+            finished = "double";
+            elapsedTimeWaypoint = "double";
+        end
+
+        function [waypoint, finished, elapsedTimeWaypoint] = isOutputComplexImpl(obj)
+            % Retourne false pour chaque port de sortie avec des données non complexes
+            waypoint = false;
+            finished = false; 
+            elapsedTimeWaypoint = false;
+        end
+
+        function [waypoint, finished, elapsedTimeWaypoint] = isOutputFixedSizeImpl(obj)
+            % Retourne true pour chaque port de sortie avec une taille fixe
+            waypoint = true;
+            finished = true;
+            elapsedTimeWaypoint = true;
         end
 
         function resetImpl(obj)
